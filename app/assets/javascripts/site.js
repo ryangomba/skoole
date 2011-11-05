f9807b152fe501c7a71fa19777cef90eb4dfcc2b
Ryan Gomba <ryan@ryangomba.com>
2011-11-05 07:05:08 -0400

    fucking hipsters

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
 	$('.overlay').click(function() {
 		$(this).fadeOut()
 	})
+
+	$('.edit_user input').click(function() {
+		$('.edit_user').submit()
+	})
+	
+	$('.nit').click(function() {
+		$('div#account-wrapper').fadeToggle()
+	})
 	
 })
 
