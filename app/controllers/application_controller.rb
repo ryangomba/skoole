0d9c8fb4e9cae64c9a5e2ad978a435427444b9ae
Ryan Gomba <ryan@ryangomba.com>
2011-11-26 12:33:10 -0500

    subdomains

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ class ApplicationController < ActionController::Base
     def current_user
         @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
+    
+    before_filter :subdomain
+    private
+    def subdomain
+        @subdomain = request.subdomains.first
+        unless @subdomain then @subdomain = params[:sub] end
+        puts "SUB: #{@subdomain}"
+        return @subdomain
+    end
 end
