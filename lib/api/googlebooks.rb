3ebbd32134d53a777a9bd092680b7a6d3801d619
Ryan Gomba <ryan@ryangomba.com>
2011-11-28 22:50:14 -0500

    updated google books code for new HTTParty

--- lib/api/googlebooks.rb
+++ lib/api/googlebooks.rb
@@ -10,16 +10,21 @@ class GoogleBooks
     
     def self.book_for_isbn(isbn)
         puts "Getting book information for ISBN."
-        book = get('/books/v1/volumes', query: {
+        request = get('/books/v1/volumes', query: {
             q: "isbn:#{isbn}"
-        })["items"][0]["volumeInfo"]
-        Book.create(
-            isbn: isbn,
-            title: book["title"],
-            author: book["authors"][0],
-            thumbnail: book["imageLinks"]["smallThumbnail"],
-            published: book["publishedDate"]
-        )
+        })
+        if request.response.class == Net::HTTPOK
+            book = request.parsed_response['items'][0]['volumeInfo'] 
+            return Book.create(
+                isbn: isbn,
+                title: book["title"],
+                author: book["authors"][0],
+                thumbnail: book["imageLinks"]["smallThumbnail"],
+                published: book["publishedDate"]
+            )
+        else
+            return nil
+        end
     end
     
 end
